083f2c7fc0c23203da1150c14c59f022f7ad09cd
Дарья <@LAPTOP-E9K2RNCV>
2022-10-24 22:20:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+Console.WriteLine("введите год");
+int year = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите номер месяца: ");
+int mounth = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите день : ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Дата предыдущего дня: {day - 1}.{mounth}.{year}");
+Console.WriteLine($"Дата следующего дня: {day + 1}.{mounth}.{year}");
+
+if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+{
+    Console.WriteLine("год високосный");
+}
+else
+{
+    Console.WriteLine("не високосный год");
+}
